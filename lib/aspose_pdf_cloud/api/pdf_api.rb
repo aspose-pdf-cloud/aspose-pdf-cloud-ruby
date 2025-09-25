7d93f37febd338273b25c683641468edbe554886
Kirill Novinskiy <kirill.novinskiy@aspose.com>
2025-09-25 14:07:39 +0000

    sources generated v25.9.0

--- lib/aspose_pdf_cloud/api/pdf_api.rb
+++ lib/aspose_pdf_cloud/api/pdf_api.rb
@@ -16351,6 +16351,93 @@ module AsposePdfCloud
       return data, status_code, headers
     end
 
+    # Crop PDF document pages.
+    # 
+    # @param name The document name.
+    # @param pages Comma separated list of pages and page ranges. (Example: 1,3-5,8)
+    # @param rect Rectangle of document area.
+    # @param [Hash] opts the optional parameters
+    # @option opts [String] :storage The document storage.
+    # @option opts [String] :folder The document folder.
+    # @option opts [String] :password Base64 encoded password.
+    # @return [AsposeResponse]
+    def post_document_pages_crop(name, pages, rect, opts = {})
+      @api_client.request_token_if_needed
+      data, _status_code, _headers = post_document_pages_crop_with_http_info(name, pages, rect, opts)
+      rescue ApiError => error
+        if error.code == 401
+          @api_client.request_token_if_needed
+          data, _status_code, _headers = post_document_pages_crop_with_http_info(name, pages, rect, opts)
+        else
+          raise
+        end
+      return data
+    end
+
+    # Crop PDF document pages.
+    # 
+    # @param name The document name.
+    # @param pages Comma separated list of pages and page ranges. (Example: 1,3-5,8)
+    # @param rect Rectangle of document area.
+    # @param [Hash] opts the optional parameters
+    # @option opts [String] :storage The document storage.
+    # @option opts [String] :folder The document folder.
+    # @option opts [String] :password Base64 encoded password.
+    # @return [Array<(AsposeResponse, Fixnum, Hash)>] AsposeResponse data, response status code and response headers
+    def post_document_pages_crop_with_http_info(name, pages, rect, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "Calling API: PdfApi.post_document_pages_crop ..."
+      end
+      # verify the required parameter 'name' is set
+      if @api_client.config.client_side_validation && name.nil?
+        fail ArgumentError, "Missing the required parameter 'name' when calling PdfApi.post_document_pages_crop"
+      end
+      # verify the required parameter 'pages' is set
+      if @api_client.config.client_side_validation && pages.nil?
+        fail ArgumentError, "Missing the required parameter 'pages' when calling PdfApi.post_document_pages_crop"
+      end
+      # verify the required parameter 'rect' is set
+      if @api_client.config.client_side_validation && rect.nil?
+        fail ArgumentError, "Missing the required parameter 'rect' when calling PdfApi.post_document_pages_crop"
+      end
+      # resource path
+      local_var_path = "/pdf/{name}/crop".sub('{' + 'name' + '}', name.to_s)
+
+      # query parameters
+      query_params = {}
+      query_params[:'pages'] = pages
+      query_params[:'storage'] = opts[:'storage'] if !opts[:'storage'].nil?
+      query_params[:'folder'] = opts[:'folder'] if !opts[:'folder'].nil?
+      query_params[:'password'] = opts[:'password'] if !opts[:'password'].nil?
+
+      # header parameters
+      header_params = {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      # HTTP header 'Content-Type'
+      header_params['Content-Type'] = @api_client.select_header_content_type(['application/json'])
+
+      # form parameters
+      form_params = {}
+      # Fix header in file
+      post_body = nil
+
+      # http body (model)
+      post_body = @api_client.object_to_http_body(rect)
+      auth_names = ['JWT']
+      data, status_code, headers = @api_client.call_api(:POST, local_var_path,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => 'AsposeResponse')
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: PdfApi#post_document_pages_crop\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
     # Rsize PDF document.
     # 
     # @param name The document name.
